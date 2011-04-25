e4d7766403167003d86bf0291650e54125ba898e
Beat Binotto <beat.binotto@gmail.com>
2011-04-25 11:44:04 +0200

    merged

--- src/TestGit.java
+++ src/TestGit.java
@@ -7,10 +7,15 @@ public class TestGit {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Hallo Git");
-		
+
 		System.out.println("hallo from master branch");
 	
 		System.out.println("hallo from local");
+
+		System.out.println("Output 3");
+
+		System.out.println("hello form local");
+
 	}
 	
 	
